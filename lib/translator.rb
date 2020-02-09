a1f7a21858039b9560b40ccb5dd3dd383f91b59b
Owen Abbott <ocabbott@gmail.com>
2020-02-09 19:01:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,29 @@
 # require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(library)
+#  pp YAML.load_file(library)
+  answer={:get_meaning => {}, :get_emoticon => {}} 
+    for i in YAML.load_file(library).keys
+      answer[:get_meaning][i]=i
+        for n in YAML.load_file(library).values
+        answer[:get_emoticon][n] = n
+      end
+      
+      
+      
+      #reminder for when I try to decipher my own code later: YAML.load_file(library).values and .keys are both arrays, so YAML.load_file(library).values.index(i) is saying to find the index of i in said array.
+    end
+    pp answer
 end
 
-def get_japanese_emoticon
-  # code goes here
+
+def get_japanese_emoticon(library)
+  
+  
 end
 
 def get_english_meaning
+  
   # code goes here
 end
